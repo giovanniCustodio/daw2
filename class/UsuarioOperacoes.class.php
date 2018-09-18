2257164afe188dcf274b72fe953484e805748c7f
custodio <gonaincu@gmail.com>
2018-09-18 20:32:02 -0300

    commit

--- class/UsuarioOperacoes.class.php
+++ class/UsuarioOperacoes.class.php
@@ -1,13 +1,40 @@
 <?php
 
-class UsuarioOperacoes{
-    
+class UsuarioOperacoes {
+
     public function __construct() {
         //
     }
-    
-    public function registrarDados(){
-        include_once 'conexao.php';
-        
+
+    public function registrarDados() {
+        try {
+            include_once 'conexao.php';
+            $sql = "insert into usuario(nomeUsuario, emailUsuario, telefone senhaUsuario) "
+                    . "values ('', '', 0,'');";
+            
+            $preparedStatment = $connection->prepare($sql);
+
+            $preparedStatment->bindParam(":texto", $pTextoBusca);
+            $result = array();
+
+            if ($preparedStatment->execute() == TRUE) {
+                $result = $preparedStatment->fetchAll();
+//                print_r($result);
+            } else {
+                throw new PDOException("Execution Error." . $preparedStatment->errorCode() . " - " .
+                $preparedStatment->errorInfo());
+            }
+        } catch (PDOException $exc) {
+            if ((isset($connection)) && ($connection->inTransaction())) {
+                $connection->rollBack();
+            }
+            print $exc->getMessage();
+        } finally {
+            if (isset($connection)) {
+                unset($connection);
+            }
+            return $result;
+        }
     }
+
 }
